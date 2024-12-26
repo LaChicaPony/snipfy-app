d7adccefa62d36822eab0e3913b2a075f0d639f3
Jessica <181289092+LaChicaPony@users.noreply.github.com>
2024-12-25 21:15:33 -0700

    Add project files.

---
+++ Snipfy.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Snipfy.App.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Snipfy.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Snipfy.App.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
